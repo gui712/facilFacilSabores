ff137bd429705d8c96a0d4a55a1b224ae9653bdc
Guilherme <guilhermecs_30@yahoo.com.br>
2024-12-13 00:20:37 -0300

    Criando menu do app

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-print('Facil Facil Sabores!\n')
+import os
 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurante')
-print('3. Ativar Restaurante')
-print('4. Sair\n')
+def exibir_nome_programa():
+    print('Facil Facil Sabores!\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+def exibir_opcoes():
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurante')
+    print('3. Ativar Restaurante')
+    print('4. Sair\n')
 
-print(f'Você escolheu a opção {opcao_escolhida}')
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando o Programa\n')
+
+def escolher_opcao():
+    
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    if opcao_escolhida == 1:
+        print('Cadastrar Restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar Restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar Restaurante')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
