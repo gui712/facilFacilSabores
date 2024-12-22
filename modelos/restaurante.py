6972e5238fb8367434982d9c1a270abba7c68750
Guilherme <guilhermecs_30@yahoo.com.br>
2024-12-22 00:10:11 -0300

    Adicionando metodos de listar restaurantes e ativa

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,23 +1,35 @@
 class Restaurante:
+
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria} | {self.ativo}'    
+        return f'{self._nome} | {self._categoria} | {self._ativo}'    
 
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria}')    
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{"Nome do Restaurante".ljust(22)} | {"Categoria".ljust(22)} | {"Status"}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(22)} | {restaurante._categoria.ljust(22)} | {restaurante.ativo}')    
+
+    @property
+    def ativo(self):
+        return 'Ativado' if self._ativo else 'Desativado'
 
 
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('Gato Veio','Pizza')
 
+
 Restaurante.listar_restaurantes()
+
