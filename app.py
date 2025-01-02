78901b2726ff3d02b91269b5bed3762939b11f2f
Guilherme <guilhermecs_30@yahoo.com.br>
2025-01-02 01:25:39 -0300

    Criando a classe de Avaliacao

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ restaurante_praca = Restaurante('Restaurante da Praça', 'Tradicional')
 restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
 restaurante_japones = Restaurante('Japa', 'Japonesa')
 
+restaurante_praca.receber_avaliacao('Guilherme', 10)
+restaurante_praca.receber_avaliacao('Jessica', 8)
+restaurante_praca.receber_avaliacao('João', 4)
+
 restaurante_mexicano.alternar_estado()
 
 def main():
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,3 +1,5 @@
+from modelos.avaliacao import Avaliacao
+
 class Restaurante:
 
     restaurantes = []
@@ -6,6 +8,7 @@ class Restaurante:
         self._nome = nome.title()
         self._categoria = categoria.upper()
         self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -13,9 +16,9 @@ class Restaurante:
 
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{"Nome do Restaurante".ljust(22)} | {"Categoria".ljust(22)} | {"Status"}')
+        print(f'{"Nome do Restaurante".ljust(22)} | {"Categoria".ljust(22)} | {"Status".ljust(22)} | {"Avaliação"}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(22)} | {restaurante._categoria.ljust(22)} | {restaurante.ativo}')    
+            print(f'{restaurante._nome.ljust(22)} | {restaurante._categoria.ljust(22)} | {restaurante.ativo.ljust(22)} | {restaurante.media_avaliacoes}')    
 
     @property
     def ativo(self):
@@ -24,5 +27,20 @@ class Restaurante:
 
     def alternar_estado(self):
         self._ativo = not self._ativo
+    
+    def receber_avaliacao(self, cliente, nota):
+        if nota > 0 and nota <= 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return '-'
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media
+
 
 
